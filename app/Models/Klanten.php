0a30ba2854b279e170285e5ea38b0b6ae7df6778
LuukvdBilt <338255@student.mboutrecht.nl>
2026-04-10 11:00:08 +0200

    opnieuw model aangemaakt met seeder en factory deze keer

--- app/Models/Klanten.php
+++ app/Models/Klanten.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Klanten extends Model
 {
-    //
+    /** @use HasFactory<\Database\Factories\KlantenFactory> */
+    use HasFactory;
 }
